afadd54a8dc6db9de834533708b9c902d46668da
victorbrugnolo <brugnolovictor@gmail.com>
2021-04-07 22:44:06 -0300

    separating responsabilities

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from "express";
 
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
@@ -8,18 +9,16 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (request: Request, response: Response) => {
     const { name, description } = request.body;
 
-    const categoryAlreadyExists = categoriesRepository.findByName(name);
+    const createCategoriesService = new CreateCategoryService(
+        categoriesRepository
+    );
 
-    if (categoryAlreadyExists) {
-        return response.status(400).json({ error: "Category already exists" });
-    }
-
-    categoriesRepository.create({ name, description });
+    createCategoriesService.execute({ name, description });
 
     return response.status(201).send();
 });
 
-categoriesRoutes.get("/", (request: Request, response: Response) => {
+categoriesRoutes.get("/", (_: Request, response: Response) => {
     const categories = categoriesRepository.list();
 
     return response.json(categories);
