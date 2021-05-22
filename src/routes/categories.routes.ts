f11170650439e5550aef7cc4efa5afd2ea630a86
victorbrugnolo <brugnolovictor@gmail.com>
2021-05-22 16:13:48 -0300

    creating category file upload

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+    dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (request, response) => {
     return createCategoryController.handle(request, response);
@@ -11,6 +16,11 @@ categoriesRoutes.post("/", (request, response) => {
 
 categoriesRoutes.get("/", (_, response) => {
     return listCategoriesController.handle(_, response);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+    const { file } = request;
+    return response.send();
 });
 
 export { categoriesRoutes };
